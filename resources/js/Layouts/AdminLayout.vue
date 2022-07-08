ee0edcd29c9c93ac5d80d6f561ac29e89768be74
gang <67897876+miseliongang@users.noreply.github.com>
2022-07-09 01:11:18 +0430

    add admin layout for panel

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+<template>
+    <div class="dark-layout">
+        <Aside />
+        <main>
+            <Nav />
+            <section>
+                <slot></slot>
+            </section>
+        </main>
+    </div>
+</template>
+<style scoped>
+    .dark-layout {
+        background-color: #161d31;
+        height: 100vh;
+    }
+</style>
