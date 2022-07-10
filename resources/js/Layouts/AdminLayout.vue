a0437a29ae8283231f16a66eb5109708db301078
gang <67897876+miseliongang@users.noreply.github.com>
2022-07-11 00:36:36 +0430

    start aside bar

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -1,15 +1,20 @@
 <script setup>
 import Nav from '../Components/NavBarComponent.vue'
+import Aside from '../Components/AsideComponent.vue'
 </script>
 <template>
-    <div class="dark-layout">
-        <Aside />
-        <Nav />
-        <main>
-            <section>
-                <slot></slot>
-            </section>
-        </main>
+    <div class="dark-layout nav-md">
+        <div class="body">
+            <div class="main_container">
+                <Aside />
+                <Nav />
+                <main>
+                    <section>
+                        <slot></slot>
+                    </section>
+                </main>
+            </div>
+        </div>
     </div>
 </template>
 <style scoped>
